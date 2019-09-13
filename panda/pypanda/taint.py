ccf76437cfbf365fc55d0bee008b9bd0be5ee45f
Tim Leek <tleek@ll.mit.edu>
2019-09-13 10:29:04 -0400

    movement

--- panda/pypanda/taint.py
+++ panda/pypanda/taint.py
@@ -2,15 +2,22 @@
 
 from pypanda import *
 from panda_x86_helper import *
+from sys import argv, stdout
 import os
+from capstone import *
 
-panda = Panda(generic="i386")
+#md = Cs(CS_ARCH_X86, CS_MODE_64)
+
+
+
+arch = "i386" if len(argv) <= 1 else argv[1]
+panda = Panda(generic=arch)
 
 
 
 state = 0 # before snapshot load
 
-@panda.cb_after_machine_init
+@panda.cb_after_machine_init(name="init")
 def machinit(env):
         global state
 
@@ -23,11 +30,13 @@ def machinit(env):
         progress("After revert: pc=%lx" % pc)
         state = 1
 
+
+
+#init_done = False
+
 nt = 0
 
-
-
-@panda.cb_before_block_exec
+@panda.cb_before_block_exec_invalidate_opt(name="bb")
 def before_block_exec(env,tb):
         global nt
         global state
@@ -36,53 +45,54 @@ def before_block_exec(env,tb):
                 return 0
 
         pc = panda.current_pc(env)
+        progress("NEW BASIC BLOCK\n")
+        progress("---------------\n")
+        progress("pc=%x" % pc)
 
-        # we just happen to know we will encounter this bb ...
+        
+
         # we just happen to know we will encounter this pc
         label_pc = 0xc12c4648
-        if pc == label_pc:
+        if state == 1 and pc == label_pc:
                 progress("I'm at label_pc=%x" % label_pc)
                 # we just about to executed this bb and we are returning
                 # so we want to taint eax
-                panda.taint_label_reg(R_EAX, label=R_EAX)
-                panda.taint_label_reg(R_EBX, label=R_EBX)
-                panda.taint_label_reg(R_ECX, label=R_ECX)
-                panda.taint_label_reg(R_EDX, label=R_EDX)
-                state = 1
-                return 0
-
-        if state == 1:
-                tqr = panda.taint_get_reg(1)
-                for tq in tqr:
-                        print (tq)
-#IN:
-#0xc1020c9c:  mov    ecx,DWORD PTR ds:0xc13e62c4
-#0xc1020ca2:  lea    eax,[eax+ecx-0x4000]
-#0xc1020ca9:  mov    DWORD PTR [eax],edx
-#0xc1020cab:  ret
-
-
-        if (state <= 33):
-                progress("Before block exec: state=%d pc=%lx" % (state,pc))
-
-        if (state == 1):
-                assert (pc == 0xc12c4648)
+                panda.taint_label_reg(R_EAX, 13)
+                panda.taint_label_reg(R_EBX, 14)
+                panda.taint_label_reg(R_ECX, 15)
+                panda.taint_label_reg(R_EDX, 16)
+                # so we don't keep trying to label over and over
                 state = 2
 
-        if (state == 2 and pc == 0xc101dfec):
-                progress ("\nCreating 'newroot' snapshot at 0xc101dfec")
-                panda.snap("newroot")
-                state = 3
+                # this should trigger re-translation of this bb s.t. its code will 
+                # get taint instrumentation
                 return 1
 
-        if state == 3:
-                nt = nt + 1
-                if nt == 10:
-                        progress("\nRestoring 'newroot' snapshot")
-                        panda.revert("newroot", now=False)
-                        nt = 0
-                        state = 3
-                return 1
+
+        def spit_taint(reg_num):
+                taint = panda.taint_get_reg(reg_num)                
+                for offset in range(panda.register_size):
+                        tq = taint[offset]
+                        if tq is None:
+                                progress("offset=%d  NOTAINT")
+                        else:
+                                progress("offset=%d taint=%s" % (offset, tq))
+
+                
+        if panda.taint_enabled:
+                if panda.taint_check_reg(R_EAX):
+                        progress("pc=%x EAX tainted" % pc)
+                        spit_taint(R_EAX)
+                if panda.taint_check_reg(R_EBX):
+                        progress("pc=%x EBX tainted" % pc)
+                        spit_taint(R_EBX)
+                if panda.taint_check_reg(R_ECX):
+                        progress("pc=%x ECX tainted" % pc)
+                        spit_taint(R_ECX)
+                if panda.taint_check_reg(R_EDX):
+                        progress("pc=%x EDX tainted" % pc)
+                        spit_taint(R_EDX)
                         
         return 0
+
 panda.run()
